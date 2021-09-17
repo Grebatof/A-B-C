fa8e737e955110090a9eff8b231f3b831d2d1932
Igor Novikov <inovikov@rit-it.com>
2021-09-17 14:58:37 +0700

    Fast and furious 3

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -24,9 +27,18 @@ class MainActivity : AppCompatActivity() {
         button?.setOnClickListener {
             val str = editText?.text.toString()
 
+            //openActivityCustom.launch("3232")
+
             startActivityForResult(BActivity.getIntent(this, str), 404)
         }
     }
+
+    private val openActivityCustom =
+        registerForActivityResult(MyActivityContract()) { result ->
+            // parseResult will return this as string?
+            if (result != null) Log.d("!!!", "Result : $result")
+            else Log.d("!!!","No Result")
+        }
 
     override fun onActivityResult(
         requestCode: Int,
@@ -35,5 +47,20 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onActivityResult(requestCode, resultCode, data)
         Toast.makeText(this, "Убит B", Toast.LENGTH_SHORT).show()
+    }
+}
+
+class MyActivityContract : ActivityResultContract<String, String?>() {
+
+    override fun createIntent(context: Context, input: String): Intent {
+        return Intent(context, BActivity.getIntent(context, input)::class.java)/*.apply {
+            putExtra(PostActivity.ID, postId)
+        }*/
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): String? {
+        val data = intent?.getStringExtra(BActivity.ARG_B)
+        return if (resultCode == Activity.RESULT_OK && data != null) data
+        else null
     }
 }
